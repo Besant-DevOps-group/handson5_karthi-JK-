43f9788fe5e0758e2eac2f645300cd41413f1f99
unknown <karthisuthakar23@gmail.com>
2024-01-25 07:54:18 +0530

    three

--- main_test.py
+++ main_test.py
@@ -1,3 +1,11 @@
-# This program prints Hello, world!
+import unittest
+from main import add
 
-print('Hello, world!')
+class TestCalc(unittest.TestCase):
+    def testadd(self):
+        return self.assertEqual(add(3,2),5)
+    
+
+
+if __name__=='__main__':
+    unittest.main()
